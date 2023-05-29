0a32c75a1c259ff6452feed0fde8e20925ef11a1
Claudinei Santos <neibaianinho@hotmail.com>
2023-05-29 15:57:58 -0300

    feat: Adicionando contextos da aplicacao

---
+++ Data/ContactContext.cs
@@ -0,0 +1,24 @@
+﻿using ContactRegister.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactRegister.Data
+{
+    public class ContactContext : DbContext
+    {
+        public ContactContext(DbContextOptions<ContactContext> options) : base(options) { }
+
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Email> Emails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Email>()
+                .HasOne(e => e.Contat)
+                .WithMany(c => c.Emails)
+                .HasForeignKey(e => e.ContatId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
