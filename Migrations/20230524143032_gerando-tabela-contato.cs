e8552f3ef024e3d768954fb400c61cc28b09d782
Claudinei Santos <neibaianinho@hotmail.com>
2023-05-29 16:03:49 -0300

    feat: Adicionando migrations para contexto de contacts

---
+++ Migrations/20230524143032_gerando-tabela-contato.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CadastroTeste.Migrations
+{
+    public partial class gerandotabelacontato : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contatos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: true),
+                    Empresa = table.Column<string>(type: "TEXT", nullable: true),
+                    TelefonePessoal = table.Column<string>(type: "TEXT", nullable: true),
+                    TelefoneComercial = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contatos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Emails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Endereco = table.Column<string>(type: "TEXT", nullable: true),
+                    ContatoId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Emails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Emails_Contatos_ContatoId",
+                        column: x => x.ContatoId,
+                        principalTable: "Contatos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Emails_ContatoId",
+                table: "Emails",
+                column: "ContatoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Emails");
+
+            migrationBuilder.DropTable(
+                name: "Contatos");
+        }
+    }
+}
