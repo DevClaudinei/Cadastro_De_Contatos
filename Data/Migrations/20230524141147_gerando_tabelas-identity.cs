874480f14453a1a755a297432aaac9c9331c91a3
Claudinei Santos <neibaianinho@hotmail.com>
2023-05-29 16:01:36 -0300

    feat: Adicionando migrations

---
+++ Data/Migrations/20230524141147_gerando_tabelas-identity.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CadastroTeste.Data.Migrations
+{
+    public partial class gerando_tabelasidentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
