9eea9803f4364f611718c75860c712436951e709
Claudinei Santos <neibaianinho@hotmail.com>
2023-05-29 16:00:22 -0300

    feat: Adicionando configurações para o banco de dados

---
+++ Configuration/DbConfiguration.cs
@@ -0,0 +1,18 @@
+using ContactRegister.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ContactRegister.Configuration;
+
+public static class DbConfiguration
+{
+    public static void AddDbConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ContactContext>(options =>
+        {
+            options.UseSqlite(configuration.GetConnectionString("DefaultConnection"),
+                            b => b.MigrationsAssembly("CadastroTeste"));
+        }, contextLifetime: ServiceLifetime.Transient);
+    }
+}
