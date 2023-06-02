66a40a8ba5796abc0b9abbcf3bb26a71cd5ede23
Claudinei Santos <neibaianinho@hotmail.com>
2023-06-01 23:06:58 -0300

    feat: criacao de validacao para propriedades

---
+++ Validations/ContactRequest.cs
@@ -0,0 +1,27 @@
+﻿using ContactRegister.Models;
+using FluentValidation;
+
+namespace ContactRegister.Validations
+{
+    public class ContactRequest : AbstractValidator<Contact>
+    {
+        public ContactRequest()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .NotNull()
+                .Must(x => x.Split(" ").Length > 1)
+                .WithMessage("First name must contain at least one last name")
+                .Must(x => !x.ContemEspacoEmBranco())
+                .WithMessage("Name must not contain blanks")
+                .Must(x => !x.ExisteAlgumSimboloOuCaracterEspecial())
+                .WithMessage("Name must not contain special characters")
+                .Must(x => x.TemPeloMenosDoisCaracteresParaCadaPalavra())
+                .WithMessage("Invalid name. First and/or last name must contain at least two letters or more");
+
+            RuleFor(x => x.PersonalPhone)
+                .Must(x => x.IsNumberPhoneValid())
+                .WithMessage("O Cellphone needs to be in the format '(XX) XXXXX-XXXX'");                
+        }
+    }
+}
---
+++ Validations/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ContactRegister.Validations
+{
+    public static class StringExtensions
+    {
+        public static bool ContemEspacoEmBranco(this string campos)
+            => campos.Split(" ").Any(x => x == string.Empty);
+
+        public static bool ExisteAlgumSimboloOuCaracterEspecial(this string valor)
+            => valor.Replace(" ", string.Empty).Any(x => !char.IsLetter(x));
+
+        public static bool TemPeloMenosDoisCaracteresParaCadaPalavra(this string campos)
+            => !campos.Split(" ").Where(x => x.Length < 2).Any();
+
+        public static bool IsNumberPhoneValid(this string phonePersonal)
+        {
+            var expression = "^\\((?:[14689][1-9]|2[12478]|3[1234578]|5[1345]|7[134579])\\) (?:[2-8]|9[1-9])[0-9]{3}\\-[0-9]{4}$";
+            return Regex.Match(phonePersonal, expression).Success;
+        }
+    }
+}
